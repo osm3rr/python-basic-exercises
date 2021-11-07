f415e9f53531b2c47ffabf0b2191cd8803090e72
@osm3rr <parabavireo@gmail.com>
2021-11-07 14:10:31 -0400

    Exercise 1

---
+++ exercise1.py
@@ -0,0 +1,23 @@
+# Basic exercises of variables
+
+# Exercise 1: 
+# Print a box like the one below.
+
+# *******************
+# *******************
+# *******************
+# *******************
+
+# Solution 1
+
+#print( '*' * 19 )
+#print( '*' * 19 )
+#print( '*' * 19 )
+#print( '*' * 19 )
+
+# Solution 2
+# using a for loop
+lines = 4
+for i in range( lines ):
+    print( '*' * 19 )
+
